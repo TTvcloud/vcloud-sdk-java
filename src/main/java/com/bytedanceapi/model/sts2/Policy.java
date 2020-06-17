57fc7e5ec203cb635c56cf49fd43449327b95c2c
yanggang.james <yanggang.james@bytedance.com>
2020-06-17 13:23:45 +0800

    fix: ZeroPadding instead of PKCS5Padding

--- src/main/java/com/bytedanceapi/model/sts2/Policy.java
+++ src/main/java/com/bytedanceapi/model/sts2/Policy.java
@@ -1,5 +1,6 @@
 package com.bytedanceapi.model.sts2;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Data
 public class Policy {
+    @JSONField(name = "Statement")
     private List<Statement> statements = new ArrayList<>();
 
     public void addStatement(Statement statement){
